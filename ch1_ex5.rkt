7b7aee2e2b56f7a8e12d2d39b3c88677bef214da
pfgallagher <pfgallagher@gmail.com>
2019-04-24 14:48:36 -0500

    Make ch1_ex5 more dynamic

--- ch1_ex5.rkt
+++ ch1_ex5.rkt
@@ -1,10 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname ch1_ex5) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
-(define circleRadius 15)
-(define rectangleWidth 10)
-(define rectangleHeight 50)
+(define treeSize 500)
 
-(define (makeTree circleRadius rectangleWidth rectangleHeight)
-  (overlay/align "middle" "top" (circle circleRadius "solid" "green") (rectangle rectangleWidth rectangleHeight "solid" "brown")))
+(define (makeTree treeSize)
+  (overlay/xy (circle (/ treeSize 8) "solid" "green")
+              (/ treeSize 13) (/ treeSize 10)
+              (rectangle (/ treeSize 10) (/ treeSize 2) "solid" "brown")))
 
+(makeTree treeSize)
