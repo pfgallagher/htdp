ee7abaf8a3f52e3fa0bc5d3dd3365e2635b28482
pfgallagher <pfgallagher@gmail.com>
2019-04-24 16:56:59 -0500

    Complete ch2_ex17

---
+++ ch2/ex17.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex17) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define (image-classify image)
+  (cond
+    [(= (image-height image) (image-width image)) "square"]
+    [(> (image-height image) (image-width image)) "tall"]
+    [else "wide"]))
+
+(image-classify (rectangle 50 50 "solid" "green"))
+(image-classify (rectangle 50 100 "solid" "green"))
+(image-classify (rectangle 100 50 "solid" "green"))
