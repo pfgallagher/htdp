5bb5fc6fbf28eff0435913024e17ed61ddfcf5d1
pfgallagher <pfgallagher@gmail.com>
2019-04-24 16:54:50 -0500

    Complete ch2_ex16

---
+++ ch2/ex16.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex16) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define (image-area image)
+  (* (image-height image) (image-width image)))
+
+(image-area (rectangle 10 10 "solid" "green"))
