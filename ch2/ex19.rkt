a97423fd07d69517fb15f81a9d2a5d8064bf9270
pfgallagher <pfgallagher@gmail.com>
2019-04-24 17:02:07 -0500

    Complete ch2_ex19

---
+++ ch2/ex19.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex19) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define (string-insert str i strToInsert)
+  (string-append (substring str 0 i) strToInsert (substring str i)))
+
+(string-insert "helloworld" 5 "_")
+(string-insert "helloworld" 5 "")
