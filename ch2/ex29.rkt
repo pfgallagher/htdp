b9466e8669f9f26fb27b14368dbf931931469573
pfgallagher <pfgallagher@gmail.com>
2019-04-29 16:27:34 -0500

    Complete ch2_ex29

---
+++ ch2/ex29.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex29) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define averageAttendance 120)
+(define averageTicketPrice 5.0)
+(define changeInAttendance 15)
+(define changeInTicketPrice 0.1)
+(define fixedCost 0)
+(define variableCost 1.50)
+
+(define (attendees ticketPrice)
+  (- averageAttendance (* (- ticketPrice averageTicketPrice) (/ changeInAttendance changeInTicketPrice))))
+
+(define (revenue ticketPrice)
+  (* ticketPrice (attendees ticketPrice)))
+
+(define (cost ticketPrice)
+  (+ fixedCost (* variableCost (attendees ticketPrice))))
+
+(define (profit ticketPrice)
+  (- (revenue ticketPrice)
+     (cost ticketPrice)))
+
+(profit 1) 
+(profit 2)
+(profit 3) 
+(profit 4) ; -> $675
+(profit 5) 
